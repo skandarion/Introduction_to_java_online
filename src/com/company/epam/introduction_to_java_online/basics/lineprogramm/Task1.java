2ca6013a0357fc04d268b8a9b75a6c6d0515e44f
skandarion <aleksandr.gatilov@gmail.com>
2021-05-13 16:41:07 +0300

    test commit

--- src/com/company/epam/introduction_to_java_online/basics/lineprogramm/Task1.java
+++ src/com/company/epam/introduction_to_java_online/basics/lineprogramm/Task1.java
@@ -1,7 +1,13 @@
 package com.company.epam.introduction_to_java_online.basics.lineprogramm;
 
+//Найдите значение функции: z = ( (a – 3 ) * b / 2) + c.
+
 public class Task1 {
-    public double findZ (double a, double b, double c) {
+    public static void main(String[] args) {
+        double result = findZ(3,4,5);
+        System.out.println(result);
+    }
+    public static double findZ (double a, double b, double c) {
         double z;
         z = ((a - 3) * b / 2) - c;
         return z;
